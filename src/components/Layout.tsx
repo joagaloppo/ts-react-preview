939d836575ebeb155f7afb99aeca42305341d18a
Joaquin Galoppo <joaquingaloppo@gmail.com>
2023-04-17 07:25:04 -0300

    feat: Create base UI components (#1)
    * feat: Create Button component
    
    * feat: Create Input component
    
    * feat: Create Box component
    
    * chore: Install moment and clsx dependencies
    
    * chore: Add VITE_GOOGLE_URL and VITE_SERVER_URL to .env.example

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
-function Layout( { children } : { children: React.ReactNode } ) {
-    return (
-        <div className="flex h-screen bg-gray-100">
-            <div className="w-full flex items-center justify-center px-4 py-12">
-                {children}
-            </div>
-        </div>
-    );
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <div className="w-full flex items-center justify-center p-6 sm:p-12">{children}</div>
+    </div>
+  );
 }
 
 export default Layout;
