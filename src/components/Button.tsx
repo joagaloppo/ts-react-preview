ff99c99ba002dec83fb64725c6ec9951a1c1d579
joagaloppo <joaquingaloppo@gmail.com>
2023-04-18 09:35:49 -0300

    refactor: Improve box, button and input components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,36 +1,55 @@
 import { ButtonHTMLAttributes } from 'react';
 import clsx from 'clsx';
+import Spinner from './Spinner';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'filled' | 'outline';
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
   children: React.ReactNode;
+  loading?: boolean;
+  disabled?: boolean;
 }
 
-function Button({ children, variant = 'outline', size = 'sm', className, ...props }: ButtonProps) {
-  const variants = {
-    filled: 'bg-blue-500 hover:bg-blue-600 text-white',
-    outline: 'bg-white text-blue-500 border border-blue-500 hover:border-blue-600 hover:text-blue-600 hover:bg-blue-50',
-  };
+const variants = {
+  filled: (loading: boolean, disabled: boolean) =>
+    clsx(`bg-blue-500 text-white`, loading || disabled ? 'cursor-not-allowed' : `hover:bg-blue-600`),
+  outline: (loading: boolean, disabled: boolean) =>
+    clsx(
+      `bg-white text-blue-500 border border-blue-500`,
+      loading || disabled ? 'cursor-not-allowed' : `hover:border-blue-600 hover:text-blue-600 hover:bg-blue-50`
+    ),
+};
 
-  const sizes = {
-    xs: 'py-2 px-4',
-    sm: 'py-2.5 px-4',
-    md: 'py-3 px-6',
-    lg: 'py-3.5 px-8',
-    xl: 'py-4 px-10',
-  };
+const sizes = {
+  xs: 'py-2 px-4',
+  sm: 'py-2.5 px-4',
+  md: 'py-3 px-6',
+  lg: 'py-3.5 px-8',
+  xl: 'py-4 px-10',
+};
 
+const Button: React.FC<ButtonProps> = ({
+  children,
+  variant = 'outline',
+  size = 'sm',
+  loading = false,
+  disabled = false,
+  className,
+  ...props
+}) => {
   const transition = 'transition duration-300 ease-in-out';
+  const rest = 'rounded-sm text-base font-medium disabled:opacity-80';
 
   return (
     <button
-      className={clsx(variants[variant], sizes[size], transition, 'rounded-sm text-base font-medium', className)}
+      className={clsx(variants[variant](loading, disabled), sizes[size], transition, rest, className)}
+      disabled={loading || disabled}
+      aria-disabled={loading || disabled}
       {...props}
     >
-      {children}
+      {loading ? <Spinner theme={variant === 'filled' ? 'light' : 'dark'} /> : children}
     </button>
   );
-}
+};
 
 export default Button;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,39 +1,42 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, forwardRef } from 'react';
 import clsx from 'clsx';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   variant?: 'filled' | 'outline';
   padding?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  error?: string | undefined;
 }
 
-function Input({ variant = 'outline', padding = 'sm', className, ...props }: InputProps) {
-  const variants = {
-    outline: 'border border-gray-300 bg-white text-gray-900',
-    filled: 'border border-gray-300 bg-gray-100 text-gray-900',
-  };
+const variants = {
+  outline: 'border border-gray-300 text-gray-900',
+  filled: 'border border-gray-300 text-gray-900',
+};
 
-  const paddings = {
-    xs: 'py-2 px-3',
-    sm: 'py-2.5 px-3',
-    md: 'py-3 px-3',
-    lg: 'py-3.5 px-3.5',
-    xl: 'py-4 px-4',
-  };
+const paddings = {
+  xs: 'py-2 px-3',
+  sm: 'py-2.5 px-3',
+  md: 'py-3 px-3',
+  lg: 'py-3.5 px-3.5',
+  xl: 'py-4 px-4',
+};
 
-  const transition = 'transition duration-300 ease-in-out';
+const errorStyle = 'bg-red-50 border-red-800/20 text-red-800/80 placeholder-red-800/80';
+const transition = 'transition duration-300 ease-in-out';
+const rest = 'w-full rounded-sm text-base outline-none';
 
-  return (
-    <input
-      className={clsx(
-        variants[variant],
-        paddings[padding],
-        transition,
-        'w-full rounded-sm text-base outline-none',
-        className
-      )}
-      {...props}
-    />
-  );
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ variant = 'outline', padding = 'sm', className, error, ...props }, ref) => {
+    return (
+      <div>
+        <input
+          ref={ref}
+          className={clsx(variants[variant], paddings[padding], transition, rest, error && errorStyle, className)}
+          {...props}
+        />
+        {error && <p className="mt-1 text-sm text-red-800/80">{error}</p>}
+      </div>
+    );
+  }
+);
 
 export default Input;
