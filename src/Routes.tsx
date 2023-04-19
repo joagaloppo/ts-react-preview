6f30a181c731b2f5766acb813fe887124c0f053f
joagaloppo <joaquingaloppo@gmail.com>
2023-04-19 17:36:34 -0300

    refactor: Add lazy loading and Suspense, implement ProtectedRoute

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,28 +1,49 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import GoogleRedirect from './pages/redirects/GoogleRedirect';
-import VerifyEmailRedirect from './pages/redirects/VerifyEmailRedirect';
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/ResetPassword';
+import { lazy, Suspense } from 'react';
+import Layout from './components/Layout';
+import Spinner from './components/Spinner';
+import Box from './components/Box';
+
+const ProtectedRoute = lazy(() => import('./components/ProtectedRoute'));
+const Login = lazy(() => import('./pages/Login'));
+const Register = lazy(() => import('./pages/Register'));
+const Home = lazy(() => import('./pages/Home'));
+const GoogleRedirect = lazy(() => import('./pages/redirects/GoogleRedirect'));
+const VerifyEmailRedirect = lazy(() => import('./pages/redirects/VerifyEmailRedirect'));
+const ForgotPassword = lazy(() => import('./pages/ForgotPassword'));
+const ResetPassword = lazy(() => import('./pages/ResetPassword'));
 
 function AppRoutes() {
-  const isAuth = !!Cookies.get('access_token');
-
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/google" element={<GoogleRedirect />} />
-        <Route path="/verify-email" element={<VerifyEmailRedirect />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+      <Suspense
+        fallback={
+          <Layout>
+            <Box>
+              <Spinner theme="dark" className="my-24" />
+            </Box>
+          </Layout>
+        }
+      >
+        <Routes>
+          <Route path="/google" element={<GoogleRedirect />} />
+          <Route path="/verify-email" element={<VerifyEmailRedirect />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
 
-        <Route path="/login" element={isAuth ? <Navigate to="/" replace={true} /> : <Login />} />
-        <Route path="/register" element={isAuth ? <Navigate to="/" replace={true} /> : <Register />} />
-        <Route path="/" element={isAuth ? <Home /> : <Navigate to="/login" replace={true} />} />
-      </Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
